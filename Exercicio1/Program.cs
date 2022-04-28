71fbd47a3e2429e3e615767df17a539dde3d1b24
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-04-28 19:59:56 -0300

    Delete Exercicio1 directory

--- Exercicio1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-/*
- * 1) Escreva um programa em C# e no Visual Studio para
- * calcular a média aritmética entre dois números quaisquer.
- * */
-
-Console.WriteLine("Hello, World!");
