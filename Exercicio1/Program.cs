6d92d396de6571710b280cfbd76dc768a1a790e4
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-04-28 19:52:56 -0300

    Exercício 1 - dia 28-04-2022

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * 1) Escreva um programa em C# e no Visual Studio para
+ * calcular a média aritmética entre dois números quaisquer.
+ * */
+
+Console.WriteLine("Hello, World!");
