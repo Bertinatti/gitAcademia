5bcb8ffb4cf0e4d51edc473026e06f54ec7f3a04
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-04-29 20:42:41 -0300

    exercicio 7 - dia 29-04-2022

---
+++ Exercicio7/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+* 7) Escreva um programa em C# e no Visual Studio que leia o número de um vendedor, o seu salário fixo,
+* o total de vendas por ele efetuadas e o percentual que ganha sobre o total de vendas. 
+* Calcule o salário total do vendedor. Escreva o número do vendedor e o salário total do vendedor.
+* */
+
+Console.WriteLine("Exercício 7: Escreva um programa em C# e no Visual Studio que leia o número de um vendedor, o seu salário fixo,\n" + "o total de vendas por ele efetuadas e o percentual que ganha sobre o total de vendas.\n" + "Calcule o salário total do vendedor. Escreva o número do vendedor e o salário total do vendedor.");
+
+int numVend;
+float salFixo, totVendas, percVendas, salTotal;
+
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("                  BEM-VINDO                   ");
+Console.WriteLine("----------------------------------------------");
+Console.Write("Digite o seu númerdo (código) de vendedor: ");
+numVend = int.Parse(Console.ReadLine());
+Console.Write("Digite o seu salário fixo: ");
+salFixo = float.Parse(Console.ReadLine());
+Console.Write("Digite o valor total das vendas: ");
+totVendas = float.Parse(Console.ReadLine());
+Console.Write("Digite o percentual (0-100) de comissão : ");
+percVendas = float.Parse(Console.ReadLine());
+
+salTotal = salFixo + totVendas * (percVendas / 100);
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("Vendedor número: " + numVend);
+Console.WriteLine($"Esse mês você receberá: R$ {salTotal:F2}");
+Console.WriteLine("----------------------------------------------");
