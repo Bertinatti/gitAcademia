fba755cf27c1f00b5b8da46e2a0f751e877b5dc8
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-04 20:59:44 -0300

    exercicio 15 - dia 04-05-2022

---
+++ Exercicio15/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* 
+ * Ler dois números e verificar se são iguais ou diferentes.
+ **/
+
+// declaração das variáveis
+double num1, num2;
+
+// imprimindo as mensagens na tela e lendo os valores para salvar nas variáveis
+Console.Write("Digite o valor do primeiro número: ");
+num1 = double.Parse(Console.ReadLine());
+Console.Write("Digite o valor do segundo número: ");
+num2 = double.Parse(Console.ReadLine());
+
+// efetuando a testagem lógica pedida no enunciado
+if (num1 == num2)
+{
+    Console.WriteLine("Os valores {0} e {1} são iguais!", num1, num2);
+}
+else
+{
+    Console.WriteLine("Os valores {0} e {1} são diferentes!", num1, num2);
+}
