ed33dcef3caae7eb9795e9f0203e87c5c5cbfdd1
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-06 22:07:44 -0300

    exercicio 20 - dia 06-05-2022

---
+++ exerciciosRicardo/Exercicio020/Program.cs
@@ -0,0 +1,118 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* 
+ * Escreva um algoritmo para o jogo de adivinhação do número secreto. O jogador tem 3 chances e recebe dicas do tipo “é maior” ou “é menor”.
+ **/
+
+int chance1, chance2, chance3, valRandom;
+Random r = new Random();
+
+valRandom = (int) r.Next(0, 100);
+
+Console.WriteLine("=================================================================================");
+Console.WriteLine("                                   ADIVINHAÇÃO                                   ");
+Console.WriteLine("=================================================================================");
+Console.Write("Sua primeira chance de adivinha o valor de 0 a 100, faça a tentativa: ");
+chance1 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+if (chance1 == valRandom)
+{
+    Console.WriteLine("Parabéns, você acertou o número.\n");
+}
+else if (chance1 > valRandom)
+{
+
+    Console.Write("O número digitado foi maior, tente digitar um número menor.\n\n" +
+                  "Sua segunda chance de adivinha o valor de 0 a 100, faça a tentativa: ");
+    chance2 = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    if (chance2 == valRandom)
+    {
+        Console.WriteLine("Parabéns, você acertou o número.\n");
+    }
+    else if (chance2 > valRandom)
+    {
+        Console.Write("O número digitado foi maior, tente digitar um número menor.\n\n" +
+                      "Sua segunda chance de adivinha o valor de 0 a 100, faça a tentativa: ");
+        chance3 = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        if (chance3 == valRandom)
+        {
+            Console.WriteLine("Parabéns, você acertou o número.\n");
+        }
+        else
+        {
+            Console.WriteLine("Que pena, você não conseguiu dessa vez.\n");
+        }
+    }
+    else
+    {
+        Console.Write("O número digitado foi menor, tente digitar um número maior.\n\n" +
+                      "Sua segunda chance de adivinha o valor de 0 a 100, faça a tentativa: ");
+        chance3 = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        if (chance3 == valRandom)
+        {
+            Console.WriteLine("Parabéns, você acertou o número.\n");
+        }
+        else
+        {
+            Console.WriteLine("Que pena, você não conseguiu dessa vez.\n");
+        }
+    }
+}
+else
+{
+    Console.Write("O número digitado foi menor, tente digitar um número maior.\n\n" +
+                  "Sua segunda chance de adivinha o valor de 0 a 100, faça a tentativa: ");
+    chance2 = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    if (chance2 == valRandom)
+    {
+        Console.WriteLine("Parabéns, você acertou o número.\n");
+    }
+    else if (chance2 > valRandom)
+    {
+        Console.Write("O número digitado foi maior, tente digitar um número menor.\n\n" +
+                      "Sua segunda chance de adivinha o valor de 0 a 100, faça a tentativa: ");
+        chance3 = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        if (chance3 == valRandom)
+        {
+            Console.WriteLine("Parabéns, você acertou o número.\n");
+
+        }
+        else
+        {
+            Console.WriteLine("Que pena, você não conseguiu dessa vez.\n");
+
+        }
+    }
+    else
+    {
+        Console.Write("O número digitado foi menor, tente digitar um número maior.\n\n" +
+                  "Sua segunda chance de adivinha o valor de 0 a 100, faça a tentativa: ");
+        chance3 = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        if (chance3 == valRandom)
+        {
+            Console.WriteLine("Parabéns, você acertou o número.\n");
+        }
+        else
+        {
+            Console.WriteLine("Que pena, você não conseguiu dessa vez.\n");
+        }
+    }
+}
+
+Console.WriteLine("=================================================================================");
+Console.WriteLine("                                 GABARARITO: {0}                                 ", valRandom);
+Console.WriteLine("=================================================================================");
+
