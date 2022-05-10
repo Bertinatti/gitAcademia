16df01e760ccffc51e0ad634b96de4fbd760ae4d
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-09 23:46:11 -0300

    exercicio 15 - dia 04-05-2022exercicio 7 - dia 09-05-2022

--- exerciciosRicardo/Exercicio009/Program.cs
+++ exerciciosRicardo/Exercicio009/Program.cs
@@ -5,10 +5,16 @@
  * diária de um paciente diabético. Para cada valor recebido analisar e informar ao paciente o seguinte:
  * Se o valor diário for menor que 65, avisá-lo que corre risco de hipoglicemia
  * Se o valor diário for maior que 250, avisá-lo que corre risco de hiperglicemia.
+ * 
+ * Além disso, o programa deve fazer a média dos 3 valores de glicemia coletados. E se a média for 
+ * menor que 80, avisá-lo que é preciso diminuir 2 unidades de insulina. Se a média for maior que
+ * 150, avisá-lo que será necessário adicionar 2 unidades de insulina.
+ * 
  **/
 
-// declaração da matriz para receber os valores
+// declaração da matriz e media para receber os valores
 double[] glicemia = new double[3];
+double media;
 
 // efetua o looping para receber os 3 valores e fazer a checagem a cada impressão
 for (int i = 0; i < glicemia.Length; i++)
@@ -29,3 +35,17 @@ for (int i = 0; i < glicemia.Length; i++)
         Console.WriteLine("Está tudo bem, mantenha assim.");
     }
 }
+
+// efetua o cálculo da média
+media = (glicemia[0] + glicemia[1] + glicemia[2]) / 3;
+
+if (media < 80)
+{
+    Console.WriteLine("Parabéns, você pode reduzir 2 unidades de insulina.");
+}
+else if (media > 150)
+{
+    Console.WriteLine("Cuidado, você pode aumentar 2 unidades de insulina.");
+}
+
+
