2012161671f9834c3eaa9c14d813a59029073f39
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-13 21:56:34 -0300

    exercicio 12 - dia 13-05-2022

---
+++ exerciciosAlexandre/Exercicio012/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* 
+ * 12. Apresentar o total da soma dos cem primeiros números inteiros.
+ **/
+
+// declaração da variável soma e atribuição de valor
+int soma = 0;
+
+// repetição para efetuar a somatório dos números inteiros de 1 a 100
+for (int i = 1; i <= 100; i++)
+{
+    soma += i;
+}
+
+// imprime na tela o resultado da somatória
+Console.WriteLine("A soma dos cem primeiros números inteiros positivos é: {0}", soma);
