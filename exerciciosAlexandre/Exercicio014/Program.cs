b4d63d37dcc5ddb3aa1fc7276fe1d608a6e7ebe6
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-13 22:26:46 -0300

    exercicio 14 - dia 13-05-2022

---
+++ exerciciosAlexandre/Exercicio014/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* 
+ * 14. Mostrar as potências de 2 variando de 0 a 10.
+ **/
+
+// declaração da variável e atribuição de valor
+int potencia = 2;
+
+// repetição para exibir o número 2 elevado ao expoente i (0 a 10)
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"2 ^ {i} = {Math.Pow(potencia, i)}");
+}
