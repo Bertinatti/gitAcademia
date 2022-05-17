4a5ca37943144346eb0668696f9b5da917432cac
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-17 20:26:39 -0300

    exercicio 17 - dia 17-05-2022

---
+++ exerciciosAlexandre/Exercicio017/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * 17. Apresentar os quadrados dos números inteiros de 15 a 200.
+ **/
+
+// declaração da variáveis
+int exp, num;
+
+// atribuição de valor ao expoente
+exp = 2;
+
+// repetição para exibir o quadrado dos números inteiros positivo de 15 a 200.
+for (int i = 15; i <= 200 ; i++)
+{
+    num = i;
+    Console.WriteLine("O quadrado do {0} é: {1:F2}\n", num, Math.Pow(num, exp));
+}
