a647f6196bba2750a0cd181bdcc9daaa51bd8cd4
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-25 20:34:59 -0300

    exercicio teste - dia 25-05-2022

---
+++ Back-end/exerciciosOrientacaoObjetos/ClasseLivro/Livro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseLivro
+{
+    internal class Livro
+    {
+        // atributos
+        public string titulo;
+        public string autor;
+        public int paginas;
+        public int ano;
+        public int edicao;
+
+        // método construtor
+        public Livro(string titulo, string autor, int pagina, int ano, int edicao)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.paginas = pagina;
+            this.ano = ano;
+            this.edicao = edicao;
+        }
+
+        // métodos
+        public void apresentaInfoLivro()
+        {
+            Console.WriteLine("O livro {0}, do autor {1}, tem {2} páginas, foi publicado no ano de {3} e está na sua {4}º versão.", this.titulo, this.autor, this.paginas, this.ano, this.edicao);
+        }
+    }
+}
---
+++ Back-end/exerciciosOrientacaoObjetos/ClasseLivro/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// instanciando o namespace onde está a classe
+using ClasseLivro;
+
+// imprime a mensagem na tela, lê o que o usuário digitado e cria uma variável para salvar esse valor
+Console.Write("Digite o título do livro: ");
+string titulo = Console.ReadLine();
+Console.Write("Digite o autor do livro: ");
+string autor = Console.ReadLine();
+Console.Write("Digite o número de páginas do livro: ");
+int paginas = int.Parse(Console.ReadLine());
+Console.Write("Digite o ano de publicação do livro: ");
+int ano = int.Parse(Console.ReadLine());
+Console.Write("Digite o número da edição do livro: ");
+int edicao = int.Parse(Console.ReadLine());
+
+// instancia o objeto da classe Livro e preenche os parâmetros do construtor
+Livro l = new Livro(titulo, autor, paginas, ano, edicao);
+
+// chama o método para apresentar as informações do livro
+l.apresentaInfoLivro();
