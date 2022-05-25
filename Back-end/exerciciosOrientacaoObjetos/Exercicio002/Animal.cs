d22aab682e935aacfc4dcde01b6b42ca93da6f16
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-25 19:55:58 -0300

    exercicio 2 - dia 25-05-2022

---
+++ Back-end/exerciciosOrientacaoObjetos/Exercicio002/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio002
+{
+    // criação da classe animal
+    internal class Animal
+    {
+        // atributos
+        private string nome;
+        private string tipo;
+
+        // método construtor e sobrecarga 
+        public Animal()
+        {
+
+        }
+
+        public Animal(string n, string t)
+        {
+            this.nome = n;
+            this.tipo = t;
+        }      
+
+        // métodos getter e setter 
+        public string Nome { get => nome; set => nome = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+    }
+}
