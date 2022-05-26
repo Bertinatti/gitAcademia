44fd8665a7c02ddee694d74d38c85b116972f969
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-26 00:00:10 -0300

    exercicio 6 - dia 26-05-2022

---
+++ Back-end/exerciciosOrientacaoObjetos/Exercicio006/PlanoCartesiano.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio006
+{
+    internal class PlanoCartesiano
+    {
+        private int posicaoX;
+        private int posicaoY;
+
+        public PlanoCartesiano(int posicaoX, int posicaoY)
+        {
+            this.PosicaoX = posicaoX;
+            this.PosicaoY = posicaoY;
+        }
+
+        public int PosicaoX { get => posicaoX; set => posicaoX = value; }
+        public int PosicaoY { get => posicaoY; set => posicaoY = value; }
+
+        public void exibeDados()
+        {
+            Console.WriteLine("Dados do ponto no Plano Cartesiano: \n" +
+                              "[1] - Posição X: {0}\n" +
+                              "[2] - Posição Y: {1}\n",this.PosicaoX, this.PosicaoY);
+        }
+        
+        public static void exibeItensLista(List<PlanoCartesiano> lista)
+        {
+            Console.WriteLine("Coordenadas presente na lista: ");
+            foreach (PlanoCartesiano p in lista)
+            {
+                Console.WriteLine("Coordenadas do Plano Cartesiano: ({0},{1}).", p.PosicaoX, p.PosicaoY);
+            }
+        }
+    }
+}
---
+++ Back-end/exerciciosOrientacaoObjetos/Exercicio006/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * #6
+ * Crie uma classe para representar um objeto em um Plano Cartesiano. Todo objeto no plano cartesiano
+ * tem como atributos 'x' e 'y'. Dessa forma, crie os getters e setter de 'x' e 'y', o construtor básico e
+ * um método para exibir um objeto (x,y).
+ * Por fim, crie um programa em C# que cadastre 'n' objetos em uma lista (List), um método de que exiba
+ * os objetos da lista.
+ * 
+ **/
+
+using Exercicio006;
+
+List<PlanoCartesiano> listaPlanoCartesiano = new List<PlanoCartesiano>();
+int posicaoX, posicaoY;
+string resposta = "";
+
+while (resposta != "N")
+{
+    Console.Write("Digite a posição X do Plano Cartesiano: ");
+    posicaoX = int.Parse(Console.ReadLine());
+    Console.Write("Digite a posição Y do Plano Cartesiano: ");
+    posicaoY = int.Parse(Console.ReadLine());
+
+    PlanoCartesiano p = new PlanoCartesiano(posicaoX, posicaoY);
+
+    listaPlanoCartesiano.Add(p);
+
+    Console.Write("Deseja adicionar mais pontos ao Plano Cartesiano[S/N]?");
+    resposta = Console.ReadLine().ToUpper();
+
+    Console.WriteLine("Pressione uma tecla para continuar.");
+    Console.ReadKey();
+
+    Console.Clear();
+}
+
+PlanoCartesiano.exibeItensLista(listaPlanoCartesiano);
