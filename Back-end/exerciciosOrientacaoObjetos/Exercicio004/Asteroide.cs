8462c10455a398dd72bb777a204bb4ea35a80ba7
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-25 22:36:03 -0300

    exercicio 4 - dia 25-05-2022

---
+++ Back-end/exerciciosOrientacaoObjetos/Exercicio004/Asteroide.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio004
+{
+    internal class Asteroide
+    {
+        private int posicaoX;
+        private int posicaoY;
+        private int tamanho;
+        private int velocidade;
+        private int energia;
+
+        public int PosicaoX { get => posicaoX; set => posicaoX = value; }
+        public int PosicaoY { get => posicaoY; set => posicaoY = value; }
+        public int Tamanho { get => tamanho; set => tamanho = value; }
+        public int Velocidade { get => velocidade; set => velocidade = value; }
+        public int Energia { get => energia; set => energia = value; }
+
+        public Asteroide()
+        {
+
+        }
+        public Asteroide(int posicaoX, int posicaoY)
+        {
+            this.posicaoX = posicaoX;
+            this.posicaoY = posicaoY;
+        }
+
+        public Asteroide(int posicaoX, int posicaoY, int tamanho, int velocidade, int energia)
+        {
+            this.posicaoX = posicaoX;
+            this.posicaoY = posicaoY;
+            this.tamanho = tamanho;
+            this.velocidade = velocidade;
+            this.energia = energia;
+        }
+
+        public static void ExibirItensLista(List<Asteroide> lista)
+        {
+            Console.WriteLine("Os dados cadastrados na lista de asteróide são: ");
+            Console.WriteLine("==============================================================================\n" +
+                              "|   POSIÇÃO X   |   POSIÇÃO Y   |   TAMANHO   |   VELOCIDADE   |   ENERGIA   |");
+
+            foreach (Asteroide a in lista)
+            {
+                Console.WriteLine("|      {0:00}       |      {1:00}       |     {2:00}      |       {3:00}       |     {4:00}      |", a.PosicaoX, a.PosicaoY, a.Tamanho, a.Velocidade, a.Energia);
+            }
+
+            Console.WriteLine("==============================================================================\n");
+
+        }
+    }
+}
---
+++ Back-end/exerciciosOrientacaoObjetos/Exercicio004/Program.cs
@@ -0,0 +1,122 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * #4
+ * Imagine um jogo com asteroides e uma nave de ataque, que deve destruir os asteróides que voam em todas as direções. Dessa forma, crie um programa em C# que represente
+ * uma lista de asteroides que deveriam ser 'inseridos' no jogo. Para isso, faça uma classe que contenha os atributos (privados) posição x, posição y (do asteroide em
+ * um plano cartesiano), tamanho do asteroide (1 a 10), velocidade do asteroide (1 a 5) e energia (1 a 5). Para esses atributos privados, gerar os métodos de acesso
+ * (propriedades getter e setter). Além disso, implementar 3 construtores (sobrecarga): um vazio, outro passando todos os parâmetros de um objeto tipo Asteroide, e um 
+ * terceiro que constrói um asteroide com posição x e posição y. 
+ * O programa principal (main) deve conter a lista de Asteroides, preenchida pelo usuário. Nesse programa principal, construir um método de classe que exiba todos os
+ * asteróides da lista.
+ * 
+ **/
+
+// instanciando o namespace que possui a classe
+using Exercicio004;
+
+// declaração das variáveis e da lista do tipo da classe
+List<Asteroide> listaAsteroides = new List<Asteroide>();
+int posicaoX, posicaoY, tamanho, velocidade, energia;
+string resposta = "";
+
+// repetição que se encerra quando o usuário responde "N"
+while (resposta != "N")
+{
+    // imprime a mensagem na tela, lê o valor digitado pelo usuário e salva na variável
+    Console.Write("Digite a posição X do asteróide: ");
+    posicaoX = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    Console.Write("Digite a posição Y do asteróide: ");
+    posicaoY = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    // repetição infinita para validar a entrada de dados
+    while (true)
+    {
+        // imprime a mensagem na tela, lê o que o usuário digitou e salva na variável temporaria
+        Console.Write("Digite o tamanho do asteróide[1~10]: ");
+        int tmpTamanho = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        // verifica se o valor digitado é válido, salva na variável e quebra o laço de repetição infinita
+        if(tmpTamanho >= 1 && tmpTamanho <= 10)
+        {
+            tamanho = tmpTamanho;
+            break;
+        }
+        // caso não seja válido, pede para que o usuário digite um valor válido
+        else
+        {
+            Console.WriteLine("O tamanho digitado é inválido.\n");
+        }
+    }
+
+    // repetição infinita para validar a entrada de dados
+    while (true)
+    {
+        // imprime a mensagem na tela, lê o que o usuário digitou e salva na variável temporaria
+        Console.Write("Digite a velocidade do asteróide[1~5]: ");
+        int tmpVelocidade = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        // verifica se o valor digitado é válido, salva na variável e quebra o laço de repetição infinita
+        if (tmpVelocidade >= 1 && tmpVelocidade <= 5)
+        {
+            velocidade = tmpVelocidade;
+            break;
+        }
+        // caso não seja válido, pede para que o usuário digite um valor válido
+        else
+        {
+            Console.WriteLine("A velocidade digitada é inválida.\n");
+        }
+    }
+
+    // repetição infinita para validar a entrada de dados
+    while (true)
+    {
+        // imprime a mensagem na tela, lê o que o usuário digitou e salva na variável temporaria
+        Console.Write("Digite a energia do asteróide[1~5]: ");
+        int tmpEnergia = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        // verifica se o valor digitado é válido, salva na variável e quebra o laço de repetição infinita
+        if (tmpEnergia >= 1 && tmpEnergia <= 5)
+        {
+            energia = tmpEnergia;
+            break;
+        }
+        // caso não seja válido, pede para que o usuário digite um valor válido
+        else
+        {
+            Console.WriteLine("A energia digitada é inválida.\n");
+        }
+    }
+
+    // instancia um objeto da classe Asteroide e usa o construtor para adicionar os atributos
+    Asteroide a = new Asteroide(posicaoX, posicaoY, tamanho, velocidade, energia);
+
+    // adiciona o objeto a lista 
+    listaAsteroides.Add(a);
+
+    // imprime a mensagem na tela, lê o que o usuário digitou e salva na variável para verificação da repetição
+    Console.Write("Digite cadastrar mais uma nota[S/N]? ");
+    resposta = Console.ReadLine().ToUpper();
+    Console.WriteLine();
+
+    // imprime a mensagem na tela e espera a entrada de alguma tecla
+    Console.WriteLine("Pressione uma tecla para continuar.");
+    Console.ReadKey();
+
+    // limpa a tela do console
+    Console.Clear();
+
+}
+
+// método da classe para exibir os itens na lista
+Asteroide.ExibirItensLista(listaAsteroides);
+
+// imprime a mensagem de saída da aplicação
+Console.WriteLine("Saindo da aplicação...\n");
