23c58795d99c475fb1bc937b02cc8949c9e1b097
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-25 23:43:25 -0300

    exercico 6 - dia 25-05-2022

---
+++ Back-end/exerciciosOrientacaoObjetos/Exercicio005/Item.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio005
+{
+    internal class Item
+    {
+        // atributo
+        private string descricao;
+        private DateTime dataCriacao;
+        private float altura;
+
+        // construtor com todos os atributos
+        public Item(string descricao, DateTime dataCriacao, float altura)
+        {
+            this.descricao = descricao;
+            this.dataCriacao = dataCriacao;
+            this.altura = altura;
+        }
+
+        // métodos getters e setters
+        public string Descricao { get => descricao; set => descricao = value; }
+        public DateTime DataCriacao { get => dataCriacao; set => dataCriacao = value; }
+        public float Altura { get => altura; set => altura = value; }
+
+        // método para imprimir todos os dados na tela
+        public void ImprimeDados()
+        {
+            Console.WriteLine("Dados do item: \n" +
+                              "[1] - Descrição: {0}\n" +
+                              "[2] - Data da criação: {1:dd/MM/yyyy}\n" +
+                              "[3] - Altura: {2}\n.", this.Descricao, this.DataCriacao, this.Altura);
+        }
+
+        // método para calcular quantos dias da criação do item até o dia de hoje
+        public int RetornaDias()
+        {
+            int dias = DateTime.Today.Subtract(this.DataCriacao).Days;
+            return dias;
+        }
+    }
+}
---
+++ Back-end/exerciciosOrientacaoObjetos/Exercicio005/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * #5
+ * Crie uma classe para representar um item de cenário, com os atributos privados de:
+ *  * descrição (string),
+ *  * data de criação (date - https://docs.microsoft.com/en-us/dotnet/api/system.datetime.date?view=net-6.0) e
+ *  * altura (float). 
+ *  
+ *  Crie os métodos públicos necessários para sets e gets, o construtor básico e também um método para imprimir 
+ *  todos dados de um item de cenário. 
+ *  Por fim, crie um método para calcular a quanto tempo o item foi criado, além de um programa em C# para testar
+ *  a classe criada.
+ *  
+ **/
+
+// instancia do namespace onde está  a classe
+using Exercicio005;
+
+// criação de uma data generica apens para testagem
+DateTime data = new DateTime(2022,02,12);
+
+// criação do objeto Item e atribuição de valores com o método construtor
+Item i = new Item("Descrição", data, 100.8f);
+
+// chamada do método para imprimir os valores na tela
+i.ImprimeDados();
+
+// atribuição de valor a variável que conta os dias da criação do item 
+int dias = i.RetornaDias();
+
+// imprime a mensagem na tela com a quantidade de dias desde a criação do item
+Console.WriteLine("São {0} dias desde a criação do item.", dias);
